c8b109c16ee171be8a8313dec12aa241d18c6fdb
Richards-Tyler <tyler@Tylers-MacBook-Pro.local>
2015-04-23 17:57:51 -0600

    added toast

--- app/src/main/java/com/example/tyler/friendlymusicparty/joinBluetooth.java
+++ app/src/main/java/com/example/tyler/friendlymusicparty/joinBluetooth.java
@@ -14,6 +14,7 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 
 public class joinBluetooth extends Activity {
@@ -40,14 +41,14 @@ public class joinBluetooth extends Activity {
 
         newDevicesListView.setAdapter(mNewDevicesArrayAdapter);
 
-        /*mNewDevicesArrayAdapter.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        newDevicesListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position,
-                                    long id) {
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 
-
+                Toast.makeText(getApplicationContext(),
+                        "Button is clicked", Toast.LENGTH_LONG).show();
             }
-        });*/
+        });
 
     }
 
